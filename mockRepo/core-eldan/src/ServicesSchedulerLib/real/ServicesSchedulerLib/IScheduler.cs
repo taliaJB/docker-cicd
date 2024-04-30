b17ddbb86baa1adb1e7e767a3948b97ab72fc048
root <admin@example.com>
2024-04-30 12:34:17 +0300

    updates

---
+++ mockRepo/core-eldan/src/ServicesSchedulerLib/real/ServicesSchedulerLib/IScheduler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace Eldan.ServicesSchedulerLib
+{
+    [ServiceContract]
+    public interface IScheduler
+    {
+        [OperationContract]
+        void Refresh(string taskId);
+
+        [OperationContract]
+        void Flush();
+    }
+}

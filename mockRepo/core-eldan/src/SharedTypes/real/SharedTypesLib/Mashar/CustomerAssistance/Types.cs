deb049f56f7acfe7dc84ba846fb23c64fec27b39
root <admin@example.com>
2024-04-30 10:13:24 +0300

    updates

---
+++ mockRepo/core-eldan/src/SharedTypes/real/SharedTypesLib/Mashar/CustomerAssistance/Types.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eldan.SharedTypesLib.Mashar.CustomerAssistance
+{
+    public enum Ema2Peima4ShmiratTeumReturnValue
+    {
+        Success = 1,
+        EventNotExist = 2,
+        EventUnableToUpdate = 3
+    };
+
+    internal class Types
+    {
+    }
+}

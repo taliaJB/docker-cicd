dbb96af30d9cf7f002d85cbec6b41eaa6417d7f5
root <admin@example.com>
2024-05-01 10:22:27 +0300

    updates

---
+++ mockRepo/core-eldan/src/ServicesScheduler/real/ServicesScheduler/ManageService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Xml.Linq;
+using System.Configuration;
+using Eldan.ServicesSchedulerLib;
+using Eldan.TypeExtensions;
+
+namespace Eldan.ServicesScheduler
+{
+    public static class ManageService
+    {
+        public static void RunService(string serviceName, string taskId)
+        {
+            string uri = GetAddress(serviceName);
+
+            TimeSpan? SendTimeout = null;
+            int SendTimeoutMinutes;
+
+            if (int.TryParse(ConfigurationManager.AppSettings["SendTimeoutMinutes"], out SendTimeoutMinutes))
+                SendTimeout = new TimeSpan(0, SendTimeoutMinutes, 0);
+
+            IScheduler proxy = ServiceModelExtentions.GetProxy<IScheduler>(uri, null, null, null, SendTimeout);
+
+            proxy.Refresh(taskId);
+        }
+
+        private static string GetAddress(string serviceName)
+        {
+            XDocument doc = XDocument.Load(ConfigurationManager.AppSettings["SchedulerConfigXML"]);
+            return doc.Descendants("service").Where(x => x.Attribute("name").Value == serviceName).First().Attribute("uri").Value;
+        }
+    }
+}
---
+++ mockRepo/core-eldan/src/ServicesScheduler/real/ServicesScheduler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Eldan.Logger;
+using System.Configuration;
+
+namespace Eldan.ServicesScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                clsLogger.Write("Program:Main - No argument specified, needs to specify service name", clsLogger.enmLogType.Error);
+                return;
+            }
+
+            string taskId = null;
+            if (args.Length > 1)
+                taskId = args[1];
+
+            string[] path = ConfigurationManager.AppSettings["loggerFilePath"].Split('.');
+            ConfigurationManager.AppSettings["loggerFilePath"] = path[0] + args[0] + "." + path[1];
+
+            ServiceLogger serviceLogger = new ServiceLogger("Program:Main");
+            serviceLogger.ThrowExceptionWhenError = false;
+            serviceLogger.DoAction(ManageService.RunService, args[0], taskId);
+
+        }
+    }
+}
